fda7dedbd348c79d01929923cd5804e4b541f7b0
chadha18 <harshitchdh@gmail.com>
2026-04-02 00:35:57 +0530

    fix port variable

--- app.py
+++ app.py
@@ -619,5 +619,6 @@ if __name__ == '__main__':
     print("   Debug: True")
     print("="*80)
     
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
-app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
